db02a018b255a3f07834beef16b5f87f934888bc
Zuoyun <Zuoyun.Jin.2020@mumail.ie>
2020-12-14 20:59:49 +0000

    test2

--- src/mqtt_subscriber.py
+++ src/mqtt_subscriber.py
@@ -1,28 +1,13 @@
 import time
 import paho.mqtt.client as paho
-from firebase import firebase
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
 import psycopg2
 import psycopg2.extras
-
-
-
 
 DB_NAME='anihlpmu'
 DB_USER ='anihlpmu'
 DB_PASS ='8lCS6ZnrHsa6UE6DYEgi6gSukx8hnfwb'
 DB_HOST ='kandula.db.elephantsql.com'
 DB_PORT='5432'
-
-file_directory='C:\\Users\Administrator\\Documents\\Intel\\OpenVINO\\omz_demos_build\\intel64\\Release\\gaze_estimation_Project\\'
-#url='https://openvino-porject-default-rtdb.firebaseio.com/'
-key='openvino-porject-firebase-adminsdk-m04su-49cb856165.json'
-#firebase =firebase.FirebaseApplication(url)
-cred = credentials.Certificate(file_directory+key)
-firebase_admin.initialize_app(cred)
-db = firestore.client()
 
 #PostgreSql
 try:
@@ -49,18 +34,7 @@ def on_message(client, userdata, msg):
                ' age :' + age + ',' + \
                ' Gaze last for ' + time_last + ' seconds.')
 
-    # firebase.put("/"+counter, "timestamps", local_time)
-    # add_to_firebase(local_time, gender, age, time_last)
     add_to_PostgreSql(local_time, gender, age, time_last)
-
-def add_to_firebase(local_time,gender,age,time_last):
-    doc_ref = db.collection('gaze_data').document(local_time)
-    doc_ref.set({
-        'timestamps':local_time,
-        'gender': gender,
-        'age': float(age),
-        'gaze_last':  float(time_last)
-    })
 
 def add_to_PostgreSql(local_time,gender,age,time_last):
     # PostgreSql
@@ -75,24 +49,11 @@ def add_to_PostgreSql(local_time,gender,age,time_last):
     print('Data inserted successfully')
     conn.close()
 
-# stream Firebase
-# emp_ref = db.collection('gaze_data')
-# docs = emp_ref.stream()
-# for doc in docs:
-#     print('{} => {}'.format(doc.id,doc.to_dict()))
-
-#
 cur.execute("SELECT * FROM GAZEDATA")
 rows = cur.fetchall()
 for data in rows:
     print("timestamps : ", data[3],", age : ",data[1],", gender : ", data[2],", time_last : ", data[4])
 print("The total number of people looked: " ,len(rows))
-
-
-
-
-
-
 
 #MQTT
 client = paho.Client()
